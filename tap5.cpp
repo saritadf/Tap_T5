8890196f0c799712a88c2679affaf622ea4bc4dc
saritadf <sarahdresdenf@gmail.com>
2014-09-28 21:22:56 -0430

    carga la entrada en una matriz

--- tap5.cpp
+++ tap5.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <istream>
 #include <vector>
 #include <queue>
 #include <list>
-
+#include<string.h>
+#include<string>
+#include<math.h>
 
 using namespace std;
-
+	char Mapa[10][10];
+	int iinicial, jinicial;
 
 struct NODO{
 int id;
@@ -19,6 +23,7 @@ NODO * parent;
 
 } ;
 /*
+
 int GetNodeHeuristic(NODO nodonuevo){return 0;}
 
 
@@ -28,12 +33,13 @@ bool Astar()
     list <NODO> CLOSE;
 
 	NODO node;
- 
+
     node.costo = 0;
     node.heuristic = GetNodeHeuristic( node );
     node.f = node.costo + node.heuristic;
     node.parent = NULL;
-    
+	node.char=Mapa[iinicial][jinicial];
+    node.id=0;
     CLOSE.insert(CLOSE.begin(),node);
 
 	while(OPEN.empty()){
@@ -45,16 +51,16 @@ bool Astar()
 			return 1;
 		}
 
-		
+
 		NodePushSuccessors( OPEN, CLOSE, node ); // see below
 		CLOSE.push_back(node); // as we have now examined this node
-	} 
-	
+	}
+
 	return 0;
 
 }
 bool BuscarNodo(list<NODO> CLOSE, NODO X ){
-	
+
 	return 0;  }
 
 void NodePushSuccessors( priority_queue<NODO> OPEN, list<NODO> CLOSE, NODO parent_node )
@@ -66,18 +72,18 @@ void NodePushSuccessors( priority_queue<NODO> OPEN, list<NODO> CLOSE, NODO paren
 			nodonuevo.costo = 1+ parent_node.costo;
 			nodonuevo.heuristic = GetNodeHeuristic(nodonuevo);
 			nodonuevo.f = nodonuevo.costo + nodonuevo.heuristic;
-		
-		/*	// OJO: O(1) si es posible
+
+			// OJO: O(1) si es posible
 			bool existe= BuscarNodo(CLOSE, nuevonodo);
 			if (nuevonodo.i== the nodonuevo is on CLOSED but the node on CLOSED has a lower ’f’
 			{
-			
+
 			}
 		// OJO: O(1) si es posible
 			if the nodonuevo is on OPEN but the node on OPEN has a lower ’f’
 			{
 				continue;
-			}*/
+			}
 				remove nodonuevo from the OPEN list if it is on it
 				remove nodonuevo from the CLOSED list if it is on it
 				nodonuevo.parent = parent_node
@@ -87,9 +93,45 @@ void NodePushSuccessors( priority_queue<NODO> OPEN, list<NODO> CLOSE, NODO paren
 */
 int main( )
 {
+	char entrada[100];
+	int x=0;
+	FILE *f;
+	f=fopen("entrada.txt","r");
 
-    
-    
+	fgets(entrada, sizeof entrada, f);
+	int tam=strlen(entrada);
+	tam=pow(tam,0.5);
+	printf(" %i \n", tam);
+	fclose(f);
+	int a;
+
+	for (int i=0; i<tam;i++)
+	{
+
+		for (int j=0; j<tam;j++)
+		{
+			Mapa[i][j]=entrada[x];
+
+			if (entrada[x]=='J'){
+				iinicial=i;
+				jinicial=j;
+			}
+			x++;
+
+		}
+	}
+	//IMPRIMIR MATRIZ
+   for (int i=0; i<tam;i++)
+	{
+		for (int j=0; j<tam;j++)
+		{
+			printf ("%c ",Mapa[i][j]);
+		}
+		printf("\n");
+	}
+
+   printf ("%i %i ",iinicial,jinicial);
+  	scanf_s("%i",&a);
 return 0;
 }
 /*
@@ -115,11 +157,11 @@ main()
 17
    L.push_back(6);
 18
- 
+
 19
    list<int>::iterator i;
 20
- 
+
 21
    for(i=L.begin(); i != L.end(); ++i) cout << *i << " ";
 22
